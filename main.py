4110e39ad661da25b589c879248ad455fbf46b27
Cristian Morales <crisdavidmm95.dev@gmail.com>
2023-06-07 21:03:51 -0500

    add more endpoints as example, include documentation

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from typing import Optional, Union, List
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, Query
 from pydantic import BaseModel
 
-app = FastAPI()
+app = FastAPI(
+		title="FastAPI Tutorial LMA",
+		description="LMA for managing students and courses",
+		version="1.0.0",
+		contact={
+			"name" : "Cristian",
+			"email": "email@example.com",
+		},
+		license_info={
+			"name": "Apache 2.0",
+		}
+)
 
 users = []
 
@@ -27,3 +38,11 @@ async def create_user(user: User):
 @app.get("/items/{item_id}")
 async def read_item(item_id: int, q: Union[str, None] = None):
 	return {"item_id": item_id, "q": q}
+
+
+@app.get("/users/{id}")
+async def get_user(
+		id: int = Path(..., description="The ID of the user to get."),
+		q: str = Query(None, max_length=5)
+):
+	return {"user": users[id], "q": q}
